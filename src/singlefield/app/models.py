0dfac96ddb92152fc6c2105a3d7d0765290e8504
Hanne Moa <hanne.moa@sikt.no>
2026-04-10 10:28:21 +0200

    Add help text to misc field

--- src/singlefield/app/models.py
+++ src/singlefield/app/models.py
@@ -5,7 +5,11 @@ class Book(models.Model):
     title = models.CharField(max_length=255)
     author = models.CharField(max_length=255)
     year = models.PositiveSmallIntegerField()
-    misc = models.JSONField(default=dict, blank=True)
+    misc = models.JSONField(
+        help_text='Stored as JSON',
+        default=dict,
+        blank=True,
+    )
 
     def get_misc_keys(self):
         return tuple(self.object.misc.keys())
